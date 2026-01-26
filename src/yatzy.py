9f1b531ff08c77bb772bfe584ab4d5ee37f5e779
DalilaManu <dmoliveira1205@gmail.com>
2026-01-26 14:17:14 +0100

    refactor: Refactoring Yatzy

--- src/yatzy.py
+++ src/yatzy.py
@@ -8,7 +8,7 @@ class Yatzy:
     NUMBER_OF_FACES = 6
 
     @staticmethod
-    def chance_score(*dice):
+    def chance_score(*dice): 
         return sum(dice)
     """
      Smell: Long Function
@@ -17,11 +17,11 @@ class Yatzy:
 
     @staticmethod
     def yatzy(dice):
-        dice_value_frequencies = [0] * (len(dice) + 1) #Initialize frequency list
+        dice_value_frequencies = [Yatzy.ZERO_POINTS] * (len(dice) + Pips.ONE.value) #Initialize frequency list
         for die in dice: #Count occurrences of each die value
-            dice_value_frequencies[die - 1] += 1 #Increment count for this die value
+            dice_value_frequencies[die - Pips.ONE.value] += Pips.ONE.value #Increment count for this die value
         for frequency in range(len(dice_value_frequencies)): #Check for Yatzy condition
-            if dice_value_frequencies[frequency] == 5: #If any die value appears 5 times
+            if dice_value_frequencies[frequency] == Yatzy.NUMBER_OF_DICE: #If any die value appears 5 times
                 return Yatzy.FIFTY_POINTS #Return Yatzy score
         return Yatzy.ZERO_POINTS
     """
@@ -34,7 +34,7 @@ class Yatzy:
     
 
     @staticmethod
-    def ones(*dice): # Calculate score for ones
+    def ones(*dice): # Calculate score for ones   
         ONE = Pips.ONE.value #Get the pip value for ones
         return dice.count(ONE) * ONE #Count and multiply by pip value
     """
@@ -129,6 +129,7 @@ class Yatzy:
     Smell: Long Function
     Common Refactorings: Extract Function        
     """
+    
 
 
 
@@ -141,53 +142,15 @@ class Yatzy:
 
 
     @staticmethod
-    def two_pair(d1, d2, d3, d4, d5):
-        counts = [0] * 6
-        counts[d1 - 1] += 1
-        counts[d2 - 1] += 1
-        counts[d3 - 1] += 1
-        counts[d4 - 1] += 1
-        counts[d5 - 1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6 - i - 1] >= 2):
-                n = n + 1
-                score += (6 - i)
+    def four_of_a_kind():
 
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
 
-    @staticmethod
-    def four_of_a_kind(_1, _2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[_1 - 1] += 1
-        tallies[_2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i + 1) * 4
-        return 0
+       
 
-    @staticmethod
-    def three_of_a_kind(d1, d2, d3, d4, d5):
-        t = [0] * 6
-        t[d1 - 1] += 1
-        t[d2 - 1] += 1
-        t[d3 - 1] += 1
-        t[d4 - 1] += 1
-        t[d5 - 1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i + 1) * 3
-        return 0
 
-    @staticmethod
-    def smallStraight(d1, d2, d3, d4, d5):
+
+     @staticmethod
+     def smallStraight(d1, d2, d3, d4, d5):
         tallies = [0] * 6
         tallies[d1 - 1] += 1
         tallies[d2 - 1] += 1
